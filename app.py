a18dcc04db9341a231c6a2ced804c43da8288a04
符建锋 <gzfujianfeng@corp.netease.com>
2018-12-18 11:35:24 +0800

    make index and test separated

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+	return render_template('index.html')
+
+
+@app.route('/test')
+def hello_world():
 	return render_template('test.html')
 
 
